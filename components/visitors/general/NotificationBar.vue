7e37a521e679289300f4ef3919098e6a17b6ec8c
Andy Giffen <andy@giffen.me>
2020-05-31 11:18:31 -0400

    Initial Commit

---
+++ components/visitors/general/NotificationBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <span>
+        <div
+            v-for="i in notificationErrors"
+            :key="i.id"
+            :class="'full-background ' + i.color"
+        >
+            {{i.text}}
+        </div>
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'NotificationBar',
+    data: () => ({
+        notificationErrors: [{ 'color': 'red', id: 0, text: 'Oh, we are having some issues' }]
+    })
+}
+</script>
+
+<style scoped>
+    div {
+        padding: 0px;
+    }
+    .full-background {
+        padding: 10px;
+        z-index: 0;
+        position: relative;
+        text-align: center;
+        color: white;
+    }
+</style>
