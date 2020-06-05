86ff1f2f81721888dcfee88dd7d0dbe61bbb62ba
Andy Giffen <andy@giffen.me>
2020-06-05 14:32:18 -0400

    Add SiteStatus as per example

---
+++ components/visitors/SiteStatus.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div class="inner">
+      <h2>{{ site.title }}</h2>
+      <IndicatorIcon
+        class="rightfloat"
+        :status="site.status"
+        :text="false"
+      />
+    </div>
+    <div
+      v-for="j in site.uptime_set"
+      :key="j.id"
+      class="uptime"
+    >
+      <div :class="'small-square-indicator ' + j.status" />
+    </div>
+  </div>
+</template>
+
+<script>
+import IndicatorIcon from './general/IndicatorIcon'
+export default {
+  name: 'SiteStatus',
+  components: { IndicatorIcon },
+  props: {
+    site: {
+      type: Object,
+      default: () => { return {} }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.rightfloat {
+  float: right;
+  margin-top: 13px;
+}
+h2 {
+  display: inline-block;
+  width: 50%;
+}
+.small-square-indicator {
+  width: 100%;
+  height: 5px;
+  display: inline-block;
+}
+.uptime {
+  display: inline-block;
+  width: 3.33%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,46 @@
 <template>
-    <div>
-        <div class="cards">
-            <div
-                v-if="openIncidents.length > 0" 
-                class="card incidents" 
-            >
-                <div
-                    v-for="i in openIncidents"
-                    :key="i.id"
-                    class="incident_item"
-                >
-                    <nuxt-link :to="{ name: 'incident-id', params: {id: i.id}}">
-                        <IncidentListItem :incident="i" />
-                    </nuxt-link>
-                </div>
-            </div>
+  <div>
+    <div class="cards">
+      <div
+        v-for="site in sites"
+        :key="site.id"
+        class="card status"
+      >
+        <SiteStatus :site="site" />
+      </div>
+      <div
+        v-if="openIncidents.length >0"
+        class="card incidents"
+      >
+        <div
+          v-for="i in openIncidents"
+          :key="i.id"
+          class="incident_item"
+        >
+          <nuxt-link :to="{ name: 'incident-id', params: { id: i.id }}">
+            <IncidentListItem :incident="i" />
+          </nuxt-link>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import IncidentListItem from '@/components/visitors/IncidentListItem.vue'
+import IncidentListItem from '@/components/visitors/IncidentListItem'
+import SiteStatus from '@/components/visitors/SiteStatus'
+
 export default {
-    components: { IncidentListItem },
-    data: () => ({
-        openIncidents: [
-            {
-                id: 1,
-                title: 'Connectivity Issues',
-                update_set: [
-                    {
-                        description: 'We have noticed some connectivity issues',
-                        date: '2019-10-02 13:00:12',
-                        status: 'Investigating' 
-                    }
-                ]
-            }
-        ]
-    })
+  components: { IncidentListItem, SiteStatus },
+  data: () => ({
+    openIncidents: [{ id: 1, title: 'Connectivity Issues', update_set: [{ description: 'We have noticed some connectivity issues', date: '2018-10-02 13:00:12', status: 'Investigating' }] }],
+    sites: [{ title: 'API', status: 'up', uptime_set: [{ date: '2018-07-12T13:42:58.085870', id: 1, response_time: 352, status: 'up' }, { date: '2018-07-12T13:43:58.085870', id: 2, response_time: 231, status: 'up' }, { date: '2018-07-12T13:44:58.085870', id: 3, response_time: 123, status: 'down' }, { date: '2018-07-12T13:45:58.085870', id: 4, response_time: 344, status: 'issue' }] }]
+  })
 }
 </script>
 
-<style>
+<style scoped>
+.card.status { margin-bottom: 10px; }
 .cards {
   max-width: 700px;
   margin: -30px auto 20px;
